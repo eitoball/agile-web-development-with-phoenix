2e4ff7c1a62a83a3b013dcc7ed333077b944b038
Eito Katagiri <eitoball@gmail.com>
2015-05-04 15:09:53 +0900

    Chapter 2 demo3

--- web/controllers/say_controller.ex
+++ web/controllers/say_controller.ex
@@ -4,7 +4,8 @@ defmodule Demo.SayController do
   plug :action
 
   def hello(conn, _params) do
-    render conn, "hello.html"
+    time = :io_lib.format("~p", [:erlang.localtime])
+    render conn, "hello.html", time: time
   end
 
   def goodbye(conn, _params) do
